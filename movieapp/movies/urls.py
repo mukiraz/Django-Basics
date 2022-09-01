3037a4ac68e19849c1cf91b3bbe142304899d2e1
mukiraz <mukiraz@gmail.com>
2022-09-01 15:40:53 +0200

    templates created

---
+++ movieapp/movies/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .  import views
+
+urlpatterns = [
+    path('', views.index, name = "index"),
+    path('movies', views.movies),
+    path('movies/<slug:slug>', views.movie_details)
+]
--- movieapp/movies/views.py
+++ movieapp/movies/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+
+def index(request):
+    return render(request, 'index.html')
+
+def movies(request):
+    return render(request, 'movies.html')
+
+def movie_details(request, slug):
+   return render(request, 'movie-details.html', {
+    'slug':slug
+   })
 
 # Create your views here.
