8fc578b0feb6b8483e765f8fcd23d59456a2448a
mukiraz <mukiraz@gmail.com>
2022-09-03 02:04:35 +0200

    pages are ok but database

--- movieapp/movieapp/settings.py
+++ movieapp/movieapp/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'tr'
 
 TIME_ZONE = 'UTC'
 
@@ -119,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- movieapp/movies/urls.py
+++ movieapp/movies/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .  import views
 
 urlpatterns = [
-    path('', views.index, name = "index"),
-    path('movies', views.movies),
-    path('movies/<slug:slug>', views.movie_details)
+    path('', views.index, name ="home_page"),
+    path('movies', views.movies, name = "movies_page"),
+    path('movies/<slug:slug>', views.movie_details, name = "movie_details")
 ]
